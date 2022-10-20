0fe23e5e6d0de6abb0c73e13b0f90d711a2842af
mdesabd <esa.ce2020@gmail.com>
2022-10-20 22:33:27 +0600

    1st input data value received

---
+++ js/banking.js
@@ -0,0 +1,30 @@
+//Handle diposit button event--
+
+// document.getElementById('diposit-button').addEventListener('click', function () {
+//     //get usar email--
+//     const dipositField = document.getElementById('diposit-input');
+//     const dipositAmmount = dipositField.value
+// console.log(dipositAmmount);
+// // এবার কনসোলে গিয়ে  ইনপুট ডাটা লিখে বাটনে তিপ দিয়ে দেখ কনসোলে ডাটা আসে কিনা তাহলে ---ওকে//
+// })
+//****************************************************************** */
+
+document.getElementById('diposit-button').addEventListener('click', function () {
+    //get usar email--
+    const dipositField = document.getElementById('diposit-input');
+    const dipositAmmount = dipositField.value
+console.log(dipositAmmount);
+// এবার কনসোলে গিয়ে  ইনপুট ডাটা লিখে বাটনে press দিয়ে দেখ কনসোলে ডাটা আসে কিনা তাহলে ---ওকে//
+})
+
+
+
+
+
+
+
+
+
+
+
+
