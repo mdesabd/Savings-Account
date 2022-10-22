fe6bb45266ba5447ddd42fca8ff9c0be4d6bd0f9
mdesabd <esa.ce2020@gmail.com>
2022-10-22 23:32:29 +0600

    savinns account done.

--- js/banking.js
+++ js/banking.js
@@ -8,40 +8,27 @@
 // // এবার কনসোলে গিয়ে  ইনপুট ডাটা লিখে বাটনে তিপ দিয়ে দেখ কনসোলে ডাটা আসে কিনা তাহলে ---ওকে//
 // })
 //****************************************************************** */
-
-
-//get diposit value=======================
+//get diposit value=============================================================
 document.getElementById('diposit-button').addEventListener('click', function () {
 
     const dipositField = document.getElementById('diposit-input');
-    const newdepositAmmount = dipositField.value
-    console.log(newdepositAmmount);
-    // এবার কনসোলে গিয়ে  ইনপুট ডাটা লিখে বাটনে press দিয়ে দেখ কনসোলে ডাটা আসে কিনা তাহলে ---ওকে//
+    const newdepositAmmount = dipositField.value;
+    // console.log(newdepositAmmount);
 
-    //deposit valu show korar jonno.......
+    // update deposite total
     const depositTotal = document.getElementById('deposit-total');
-    // console.log(depositTotal.value); //value hobe na karon innerText.
     const previousDepositAmount = depositTotal.innerText;
     const newDepositeTotal = parseFloat(previousDepositAmount) + parseFloat(newdepositAmmount);
     depositTotal.innerText = newDepositeTotal;
-    // dipositAmmount hobe okhane joma hobe---
+
+// **********************************************************************************
 
 
-
-
-
-//update acount blance---
-
-
-
-
-
-
-
-
-
-
-
+    //update acount blance---
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const newBalanceTotal = parseFloat(newdepositAmmount) + parseFloat(previousBalanceTotal);
+    balanceTotal.innerText = newBalanceTotal;
 
     //clear the deposite inputfield
     dipositField.value = '';
@@ -50,12 +37,29 @@ document.getElementById('diposit-button').addEventListener('click', function () 
 
 
 
+document.getElementById('withdraw-button').addEventListener('click', function () {
+
+    //Update update withdraw Field and button---
+    const withdrawField = document.getElementById('withdraw-input');
+    const newWithdrawAmount = withdrawField.value;
+    console.log(newWithdrawAmount);
+
+
+    //Update withdraw and balance ----
+
+    const WithdrawTotal = document.getElementById('withdray-total');
+    const previousWithdrawAmount = WithdrawTotal.innerText;
+    const withdrawTotalAmount = parseFloat(previousWithdrawAmount) + parseFloat(newWithdrawAmount);
+    WithdrawTotal.innerText = withdrawTotalAmount;
+
+    //clear the withdray inputfield
+    withdrawField.value = '';
 
 
 
+    const balanceTotal = document.getElementById('balance-total');
+    const previousBalanceTotal = balanceTotal.innerText;
+    const newBalanceTotal = parseFloat(previousBalanceTotal) - parseFloat(newWithdrawAmount);
+    balanceTotal.innerText = newBalanceTotal;
 
-
-
-
-
-
+})
